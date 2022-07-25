c1e8c9cd4fedd3be22b93c2b1552185e082a664b
Luis <lhernand3z@gmail.com>
2022-07-24 21:09:58 -0500

    refractored fashion.py into fashion-callback.py it now checks the accuracy at the end of every epoch and it will complete if reached

--- fashion-callback.py
+++ fashion-callback.py
@@ -1,11 +1,10 @@
 #Copy of Week2 colab from Coursera Intro to Tensorflow
-#this is an example on computer vision
+#this is an example on computer vision but with a using a callback class to stop
+#the training once a certain level of accuracy
 
 import tensorflow as tf
 print(tf.__version__)
 
-#Load the Fashion MNIST dataset
-fmnist = tf.keras.datasets.fashion_mnist
 
 #Label	Description
 # 0	T-shirt/top
@@ -19,31 +18,23 @@ fmnist = tf.keras.datasets.fashion_mnist
 # 8	Bag
 # 9	Ankle boot
 
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('accuracy') >= 0.88):
+            print("\nReached 88% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+callbacks = myCallback()
+
+#Load the Fashion MNIST dataset
+fmnist = tf.keras.datasets.fashion_mnist
+
 #Load the training and test split of the Fashion MNIST dataset
 (training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
 
-#Printing a training image/label, just to see the data
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-#You can put between 0 and 59999 here
-index = 42
-
-# Set number of characters per row when printing
-np.set_printoptions(linewidth=320)
-
-# Print the label and image
-print(f'LABEL: {training_labels[index]}')
-print(f'\nIMAGE PIXEL ARRAY:\n {training_images[index]}')
-
-#Visualize the image
-plt.imshow(training_images[index])
-
 #Normalize the pixel values of the train and test images
 training_images = training_images / 255.0
-#Saving the raw images for later analysis
-test_images_raw = test_images
 test_images = test_images / 255.0
 
 # Build the classification model
@@ -56,7 +47,7 @@ test_images = test_images / 255.0
 #   ReLU, it only passes values 0 or greater to the next layer
 #   Softmax, takes a list of values and scales these so the sum of all elements will be equal to 1
 model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
-                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 # Compile the module
@@ -65,19 +56,16 @@ model.compile(optimizer = tf.optimizers.Adam(),
               metrics = ['accuracy'])
 
 # Train the module
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
 
 # Evaluate the model with unseen data
 model.evaluate(test_images, test_labels)
 
 # Predicts the classifications of the test_images
 classifications = model.predict(test_images)
-print(f'\nIMAGE PIXEL ARRAY:\n {test_images_raw[0]}')
 print(classifications[0])
 
-print(f'\nIMAGE PIXEL ARRAY:\n {test_images_raw[10]}')
 print(classifications[10])
 
-print(f'\nIMAGE PIXEL ARRAY:\n {test_images_raw[42]}')
 print(classifications[42])
 
